e530f5f79af7637b173557edcde432f642626a95
sinziananicolae <sinzianna.nicolae@gmail.com>
2015-03-19 21:25:31 +0200

    Fragment Basic Details

--- workspace/src/ro/pub/cs/systems/pdsd/lab04/contactsmanager/ContactsManagerActivity.java
+++ workspace/src/ro/pub/cs/systems/pdsd/lab04/contactsmanager/ContactsManagerActivity.java
@@ -1,6 +1,8 @@
 package ro.pub.cs.systems.pdsd.lab04.contactsmanager;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,11 @@ public class ContactsManagerActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts_manager);
+		
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.add(R.id.frame1, new BasicDetailsFragment());
+		fragmentTransaction.commit();
 	}
 
 	@Override
